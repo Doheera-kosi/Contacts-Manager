f3690a4d5e633e8c00830457574d15c5e449b509
Doheera-kosi <kupourevans99@gmail.com>
2023-04-12 17:21:13 +0000

    Add updateContact methods for CRUD operations

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -45,7 +45,19 @@ const getContact = asyncHandler(async (req, res) => {
 //@route  PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Update Contact for ${req.params.id}`});
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact Not Found!")
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedContact);
 });
 
 //@desc Delete new contacts
