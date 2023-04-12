b5cd2df1464a4afc710faf6141ad398ff92d2650
Doheera-kosi <kupourevans99@gmail.com>
2023-04-12 19:13:04 +0000

    Update userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,24 @@
 const asyncHandler = require("express-async-handler")
+const User = require("../models/userModel");
 
 //@desc Register user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler( async (req, res) => {
+  const {username, email, password} = req.body;
+  if(!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const userAvailable = await User.findOne({ email });
+  if(userAvailable) {
+    res.status(400);
+    throw new Error("User registered already")
+  }
+
+  
+
   res.json({message: "Register user"});
 });
 
